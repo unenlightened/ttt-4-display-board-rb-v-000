1511ce645aa91573c627b75894f29d9fe47a7e8a
Chely Ho <adept_inept@yahoo.com>
2018-02-03 18:42:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,10 @@
 # out the current state.
 
 def display_board(board)
-  things
+  new_line = "-----------"
+  puts " #{board[0]} | #{board[1]} |  #{board[2]} "
+  puts new_line
+  puts " #{board[3]} | #{board[4]} |  #{board[5]} "
+  puts new_line
+  puts " #{board[6]} | #{board[7]} |  #{board[8]} "
 end
