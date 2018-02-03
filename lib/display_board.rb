9c5298f95b613cacca19f2674d86597af4c32433
Chely Ho <adept_inept@yahoo.com>
2018-02-03 18:45:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 def display_board(board)
   new_line = "-----------"
-  puts " #{board[0]} | #{board[1]} |  #{board[2]} "
+  puts " #{board[0]} | #{board[1]} |  #{board[2]}"
   puts new_line
-  puts " #{board[3]} | #{board[4]} |  #{board[5]} "
+  puts " #{board[3]} | #{board[4]} |  #{board[5]}"
   puts new_line
-  puts " #{board[6]} | #{board[7]} |  #{board[8]} "
+  puts " #{board[6]} | #{board[7]} |  #{board[8]}"
 end
